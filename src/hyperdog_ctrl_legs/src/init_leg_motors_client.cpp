86fc5733f5429a505539d9e6ad127c6a6e1480e7
NDHANA94 <nipun.dhananjaya@gmail.com>
2023-09-02 21:28:07 +0300

    updated with signatur

--- src/hyperdog_ctrl_legs/src/init_leg_motors_client.cpp
+++ src/hyperdog_ctrl_legs/src/init_leg_motors_client.cpp
@@ -1,3 +1,32 @@
+/**
+ * Author: Nipun Dhananjaya Weerakkodi  (nipun.dhananjaya@gmail.com)
+ * Last update: 02.09.2023
+ * 
+===========================================================================================
+MIT License                                                                               |
+                                                                                          |
+Copyright (c) 2023 Nipun Dhananjaya Weerakkodi                                            |
+                                                                                          |
+Permission is hereby granted, free of charge, to any person obtaining a copy              |
+of this software and associated documentation files (the "Software"), to deal             |
+in the Software without restriction, including without limitation the rights              |
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                 |
+copies of the Software, and to permit persons to whom the Software is                     |
+furnished to do so, subject to the following conditions:                                  |
+                                                                                          |
+The above copyright notice and this permission notice shall be included in all            |
+copies or substantial portions of the Software.                                           |
+                                                                                          |
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                |
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                  |
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE               |
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                    |
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,             |
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE             |
+SOFTWARE.                                                                                 |
+                                                                                          |
+==========================================================================================*/
+
 #include "rclcpp/rclcpp.hpp"
 #include "hyperdog_uros_interfaces/srv/init_leg_motors.hpp"
 #include "hyperdog_uros_interfaces/srv/init_leg_motors.h"
@@ -43,10 +72,10 @@ void print_motorError(int motor, int error){
     }
 }
 
-/* ==================================================================================================================
+/* =========================================================================================================
                                     INIT LEG MOTORS CLIENT
                                             MAIN
-=================================================================================================================== */
+========================================================================================================== */
 
 int main(int argc, char** argv)
 {
@@ -63,7 +92,8 @@ int main(int argc, char** argv)
     
     while (!client->wait_for_service(1s)){
         if(!rclcpp::ok()){
-            RCLCPP_ERROR(rclcpp::get_logger("InitLegMotors"), "Interupted while waiting for the service. Exiting.");
+            RCLCPP_ERROR(rclcpp::get_logger("InitLegMotors"), 
+                "Interupted while waiting for the service. Exiting.");
             return 0;
         }
         RCLCPP_INFO(rclcpp::get_logger("InitLegMotors"), "service not available, waiting again...");
